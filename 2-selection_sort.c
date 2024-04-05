17dd4bb3c3fbd7a9456c7db0341dfbf7eb46d449
elyessassi <mjoxen@gmail.com>
2024-04-05 22:30:40 +0000

    selection sort an array

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,18 +14,18 @@ void selection_sort(int *array, size_t size)
 	int current_min;
 	size_t current_min_idx;
 	size_t start_pt;
-	size_t curr_element_idx;
+	size_t curr_elem_idx;
 	size_t temp;
 
 	for (start_pt = 0; start_pt < size; start_pt++)
 	{
 		current_min = array[start_pt];
-		for (curr_element_idx = start_pt ; curr_element_idx < size ; curr_element_idx++)
+		for (curr_elem_idx = start_pt ; curr_elem_idx < size ; curr_elem_idx++)
 		{
-			if (array[curr_element_idx] < current_min )
+			if (array[curr_elem_idx] < current_min )
 			{
-				current_min = array[curr_element_idx];
-				current_min_idx = curr_element_idx;
+				current_min = array[curr_elem_idx];
+				current_min_idx = curr_elem_idx;
 			}
 		}
 		if (current_min != array[start_pt])

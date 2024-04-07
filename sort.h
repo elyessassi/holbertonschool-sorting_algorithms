2d8afac888d7fd7d5c2ae061dd9cd0bd0f0b5c7b
elyessassi <mjoxen@gmail.com>
2024-04-07 20:44:55 +0000

    header file

--- sort.h
+++ sort.h
@@ -25,5 +25,9 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+int partition(int *array, int low, int high, size_t length);
+void swap(int *i, int *j);
+void quick_sort_recursion(int *array, int low, int high, size_t length);
+void quick_sort(int *array, size_t length);
 
 #endif
